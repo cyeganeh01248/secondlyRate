4b23614aa756af288a2c5651c8dbe6830417f523
Chris Yeganeh <cyeganeh@integralads.com>
2024-08-12 14:05:44 -0400

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
+import staticAdapter from "solid-start-static";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-    plugins: [solid()],
+    plugins: [solid({ adapter: staticAdapter() })],
     base: "/secondlyRate/",
     clearScreen: false,
     server: {
